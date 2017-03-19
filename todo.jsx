981d5a22c2412991a6acf61012b38bbcf698eb4f
dev <mrgoldface@MrGoldFaces-MacBook-Pro.local>
2017-03-19 07:47:29 -0400

    update 03-18-17

--- todo.jsx
+++ todo.jsx
@@ -1,8 +1,4 @@
-// Make all login actions and state redux
-
 // listview will only update 'onEndReached' if populated first
-
-// redux: dispatch called multiple times
 
 // splashScreen: setTimeout dont work, maybe 'this' is different
 
